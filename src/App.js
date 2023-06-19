aeb3dd755ce3ca828a2b2e5b8316335a7312c3f0
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-06-19 22:44:26 +0200

    game logic change

--- src/App.js
+++ src/App.js
@@ -1,64 +1,69 @@
 import React, { useState, useEffect } from 'react';
 import './style.css';
-import {ANIMAL_LIST, ALPHABET}  from '../src/db/db';
+import { ANIMAL_LIST, ALPHABET } from '../src/db/db';
 
 export default function App() {
-  
-
   const [quessedLetters, setQuessedLetters] = useState([]);
   const [bad, setBad] = useState([]);
   const [lives, setLives] = useState(6);
-  const [passwd, setPasswd] = useState('');
+
   const [reset, setReset] = useState(false);
-  const [scoreCount, setScoreCount] = useState(0)
+  const [scoreCount, setScoreCount] = useState(0);
 
-  const RandomPasswd = () => {
+  const [passwd, setPasswd] = useState(() => {
     const random = Math.floor(Math.random() * ANIMAL_LIST.length);
-    setPasswd(ANIMAL_LIST[random].toUpperCase());
-    console.log(random, ANIMAL_LIST[random]);
-  };
+    return ANIMAL_LIST[random].toUpperCase();
+  });
 
-  useEffect(() => {
-    RandomPasswd();
-  }, [reset]);
+
+
+  useEffect(()=>{
+    console.log(passwd)
+  },[])
 
   const handleClick = (e) => {
-   
+    if (lives===0){
+      lose=true;
+      win=false;
+    }else
     if (passwd.includes(e)) {
       setQuessedLetters([...quessedLetters, e]);
     } else {
       setBad([...bad, e]);
-      setLives(lives - 1);
-    }
+      setLives(lives - 1)
+    } 
   };
 
-  const win = passwd
+let win = passwd
     .split('')
     .every((letter) => quessedLetters.includes(letter));
-  const lose = bad.length >= 6;
-  
+ let lose = lives ===0;
+
   const restart = () => {
+    const random = Math.floor(Math.random() * ANIMAL_LIST.length);
+    setPasswd(ANIMAL_LIST[random].toUpperCase());
     if (win) {
-      setScoreCount(scoreCount+1)
-    
+      
+      setScoreCount(scoreCount + 1);
       setQuessedLetters([]);
-      setReset(!reset);
-    }else {
-    setLives(6);
-    setScoreCount(0);
-    setBad([]);
-    setQuessedLetters([]);
-    setReset(!reset);
-    win: false;
-    lose: false;
+      setBad([]);
+   
+    } else {
+          
+            setLives(6);
+            setScoreCount(0);
+            setBad([]);
+            setQuessedLetters([]);
+       
+         
     }
   };
-
+ 
   return (
     <div className="App">
-      <h1>Animal Name Quiz</h1>
+       <h1>Animal Name Quiz</h1>
       <div className="lives">
-      <span className="span-lives">
+        <span className="span-lives">
           Lives : <span className="lives-num">{lives}</span>
         </span>
         <span className="span-lives">
@@ -114,7 +119,7 @@ export default function App() {
         <button className="restart" onClick={restart}>
           Restart
         </button>
-      </div>
+      </div> 
     </div>
   );
 }
